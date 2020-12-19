69118a05dda5aaaaa30bd995a15645cfcc6322c9
Ïgor Zakharov <Igor1994294@yandex.ru>
2020-12-19 11:40:45 +0300

    feat: удалить helpers

--- helpers/files.js
+++
@@ -1,8 +0,0 @@
-const fsPromises = require('fs').promises;
-
-const getDataFromFile = (pathToFile) => {
-  return fsPromises.readFile(pathToFile, {encoding: 'utf-8'})
-    .then(data => JSON.parse(data));
-}
-
-module.exports = getDataFromFile;
